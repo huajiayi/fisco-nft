a741371cf490a68e99f3ab0c51b38b86f5eef642
华嘉熠 <1601939052@qq.com>
2022-04-23 13:59:04 +0800

    first commit

---
+++ src/main/java/com/example/fisconft/FiscoNftApplication.java
@@ -0,0 +1,13 @@
+package com.example.fisconft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiscoNftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FiscoNftApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fisconft/FiscoNftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fisconft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiscoNftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
